0ca51e8b09a6aecbefdf02e30d4fda53a0d23204
David Shea <dshea@redhat.com>
2016-05-04 16:43:57 -0400

    Add tests for the top-level functions

--- tests/unittests/test_translated.py
+++ tests/unittests/test_translated.py
@@ -15,13 +15,17 @@
 # Red Hat Author(s): David Shea <dshea@redhat.com>
 
 import unittest
+import unittest.mock
 import tempfile
 import warnings
 import polib
+import os
 
 from translation_canary.translated.test_markup import test_markup
 from translation_canary.translated.test_percentage import test_percentage
 from translation_canary.translated.test_usability import test_usability
+
+from translation_canary.translated import testFile, testSourceTree
 
 # convert a polib.POFile into a NamedTemporaryFile
 def pofile(poobj):
@@ -111,3 +115,109 @@ class TestUsability(unittest.TestCase):
 
         with pofile(poobj) as p:
             self.assertRaises(Exception, test_usability, p.name)
+
+# Test the top-level functions
+# fake tests for testing with
+def _true_test(_p):
+    pass
+
+def _false_test(_p):
+    raise AssertionError("no good")
+
+def _picky_test(p):
+    if os.path.basename(p).startswith("p"):
+        raise AssertionError("I don't like this one")
+
+class TestTestFile(unittest.TestCase):
+    @unittest.mock.patch("translation_canary.translated._tests", [_true_test])
+    def test_success(self):
+        with pofile_from_entry() as p:
+            self.assertTrue(testFile(p.name))
+
+    @unittest.mock.patch("translation_canary.translated._tests", [_false_test])
+    def test_failure(self):
+        with pofile_from_entry() as p:
+            self.assertFalse(testFile(p.name))
+
+    @unittest.mock.patch("translation_canary.translated._tests", [_true_test])
+    def test_release_mode_success(self):
+        with pofile_from_entry() as p:
+            self.assertTrue(testFile(p.name, releaseMode=True))
+            self.assertTrue(os.path.exists(p.name))
+
+    @unittest.mock.patch("translation_canary.translated._tests", [_false_test])
+    def test_release_mode_failure(self):
+        p = tempfile.NamedTemporaryFile(suffix='.po', delete=False)
+        try:
+            # testFile should return True but delete the file
+            self.assertTrue(testFile(p.name, releaseMode=True))
+            self.assertFalse(os.path.exists(p.name))
+        finally:
+            try:
+                p.close()
+                os.unlink(p.name)
+            except FileNotFoundError:
+                pass
+
+    @unittest.mock.patch("translation_canary.translated._tests", [_false_test])
+    def test_release_mode_failure_with_lingua(self):
+        with tempfile.TemporaryDirectory() as d:
+            open(os.path.join(d, "test.po"), "w").close()
+            with open(os.path.join(d, "LINGUAS"), "w") as linguas:
+                linguas.write("test other\n")
+
+            # Check that test.po is removed and test is removed from LINGUAS
+            self.assertTrue(testFile(os.path.join(d, "test.po"), releaseMode=True))
+            self.assertFalse(os.path.exists(os.path.join(d, "test.po")))
+
+            with open(os.path.join(d, "LINGUAS"), "r")  as linguas:
+                self.assertEqual(linguas.read().strip(), "other")
+
+@unittest.mock.patch("translation_canary.translated._tests", [_picky_test])
+class TestTestSourceTree(unittest.TestCase):
+    def setUp(self):
+        self.tmpobj = tempfile.TemporaryDirectory()
+        self.tmpdir = self.tmpobj.name
+
+    def tearDown(self):
+        self.tmpobj.cleanup()
+
+    def test_success(self):
+        open(os.path.join(self.tmpdir, "aa.po"), "w").close()
+        open(os.path.join(self.tmpdir, "ab.po"), "w").close()
+        self.assertTrue(testSourceTree(self.tmpdir))
+
+    def test_some_failure(self):
+        open(os.path.join(self.tmpdir, "aa.po"), "w").close()
+        open(os.path.join(self.tmpdir, "pa.po"), "w").close()
+        self.assertFalse(testSourceTree(self.tmpdir))
+
+    def test_all_failure(self):
+        open(os.path.join(self.tmpdir, "pa.po"), "w").close()
+        open(os.path.join(self.tmpdir, "pb.po"), "w").close()
+        self.assertFalse(testSourceTree(self.tmpdir))
+
+    def test_release_mode_success(self):
+        open(os.path.join(self.tmpdir, "aa.po"), "w").close()
+        open(os.path.join(self.tmpdir, "ab.po"), "w").close()
+        self.assertTrue(testSourceTree(self.tmpdir, releaseMode=True))
+
+    def test_release_mode_failure(self):
+        open(os.path.join(self.tmpdir, "aa.po"), "w").close()
+        open(os.path.join(self.tmpdir, "pa.po"), "w").close()
+        self.assertTrue(testSourceTree(self.tmpdir, releaseMode=True))
+        self.assertTrue(os.path.exists(os.path.join(self.tmpdir, "aa.po")))
+        self.assertFalse(os.path.exists(os.path.join(self.tmpdir, "pa.po")))
+
+    def test_release_mode_failure_with_lingua(self):
+        open(os.path.join(self.tmpdir, "aa.po"), "w").close()
+        open(os.path.join(self.tmpdir, "pa.po"), "w").close()
+        with open(os.path.join(self.tmpdir, "LINGUAS"), "w") as l:
+            l.write("aa pa\n")
+
+        self.assertTrue(testSourceTree(self.tmpdir, releaseMode=True))
+        self.assertTrue(os.path.exists(os.path.join(self.tmpdir, "aa.po")))
+        self.assertFalse(os.path.exists(os.path.join(self.tmpdir, "pa.po")))
+
+        with open(os.path.join(self.tmpdir, "LINGUAS")) as l:
+            self.assertEqual(l.read().strip(), "aa")
