51ea933140a011ccafcdc8f8493d79c3cc308a12
David Shea <dshea@redhat.com>
2016-05-09 08:41:38 -0400

    Fixed a couple license comments missing newlines.

--- tests/unittests/test_translated.py
+++ tests/unittests/test_translated.py
@@ -10,7 +10,9 @@
 # GNU General Public License along with this program; if not, write to the
 # Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
-# source code or documentation are not subject to the GNU General Public # License and may only be used or replicated with the express permission of # Red Hat, Inc.
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
 #
 # Red Hat Author(s): David Shea <dshea@redhat.com>
 
